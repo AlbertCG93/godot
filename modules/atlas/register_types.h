abd0d315d01be8d1e107abf18fcf3b30299158c4
AlbertCG93 <albert.cerro.gutierrez@gmail.com>
2020-11-15 17:49:40 +0100

    WIP

---
+++ modules/atlas/register_types.h
@@ -0,0 +1,7 @@
+#ifndef REGISTER_ATLAS_TYPES_H
+#define REGISTER_ATLAS_TYPES_H
+
+void register_atlas_types();
+void unregister_atlas_types();
+
+#endif
---
+++ modules/atlas/world.cpp
@@ -0,0 +1,28 @@
+#include "modules/atlas/world.h"
+
+World::World() {
+
+}
+
+World::World(int x, int y) {
+    representation = std::vector<std::vector<uint8_t>>(x, std::vector<uint8_t>(y));
+}
+
+World::~World() {
+
+}
+
+void World::set_value(int i, int j, uint8_t value) {
+    representation[i][j] = value;
+}
+
+uint8_t World::get_value(int i, int j) {
+    return representation[i][j];
+}
+
+void World::_bind_methods() {
+    ClassDB::bind_method(D_METHOD("set_value", "i", "j", "value"), &World::set_value);
+    ClassDB::bind_method(D_METHOD("get_value", "i", "j"), &World::get_value);
+    //ClassDB::bind_method(D_METHOD("set_value"), &World::set_value);
+    //ClassDB::bind_method(D_METHOD("get_value"), &World::get_value);
+}
